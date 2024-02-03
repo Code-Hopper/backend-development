084e6aa159873bf64a9a8fd2242ed5389da917a1
code_hopper <ameykhondekar01@gmail.com>
2024-02-03 09:06:31 +0530

    create api routes for various purpose

--- backend-apis/controllers/controller.js
+++ backend-apis/controllers/controller.js
@@ -1,15 +1,17 @@
+import e from "express"
+
 let data = [
 
-    { id: 1, language: "c", duration: "1 Month", level: "hard" },
-    { id: 2, language: "C++", duration: "1.5 Month", level: "hard" },
-    { id: 3, language: "JavaScript", duration: "2 Months", level: "easy" },
-    { id: 4, language: "Ejs", duration: "1 Day", level: "hard" },
-    { id: 5, language: "node.js", duration: "Instantly", level: "hard" },
-    { id: 6, language: "express", duration: "1 Month", level: "very hard" },
-    { id: 7, language: "HTML", duration: "1 Month", level: "very easy" },
-    { id: 8, language: "CSS", duration: "1 Month", level: "Medium Hard" },
-    { id: 9, language: "Bootstrap", duration: "5 Days", level: "easy" },
-    { id: 10, language: "React", duration: "Infinity", level: "Exterm Hard" },
+    { id: 1, language: "c", duration: 1, level: "hard" },
+    { id: 2, language: "C++", duration: 1.5, level: "hard" },
+    { id: 3, language: "JavaScript", duration: 2 , level: "easy" },
+    { id: 4, language: "Ejs", duration: 1 , level: "hard" },
+    { id: 5, language: "node.js", duration: 0, level: "hard" },
+    { id: 6, language: "express", duration: 1, level: "very hard" },
+    { id: 7, language: "HTML", duration: 3, level: "very easy" },
+    { id: 8, language: "CSS", duration: 2.5, level: "Medium Hard" },
+    { id: 9, language: "Bootstrap", duration: 2, level: "easy" },
+    { id: 10, language: "React", duration: 1000, level: "Exterm Hard" },
 
 ]
 
@@ -20,23 +22,95 @@ let GetHome = (req, res) => {
 
 let GetLanguages = (req, res) => {
 
-    let randomID = Math.round(Math.random() * 9 + 1)
+    if (req.query.level) {
+        // filter the result
 
-    console.log(randomID)
+        let levelType = req.query.level
+
+        let newData = data.filter((element) => {
+            return element.level == levelType
+        })
+
+        res.status(200).json(newData)
+
+    } else if (req.query.duration) {
+        // filter the result
+
+        let durationType = req.query.duration
+
+        let newData = data.filter((element) => {
+
+            // clear and slipt the number from duration
+
+            return element.duration <= durationType
+        })
+
+        res.status(200).json(newData)
+
+    }
+    else {
+        // return the random course
+
+        let randomID = Math.round(Math.random() * 9 + 1)
+
+        console.log(randomID)
 
 
-    let randomElemenet;
+        let randomElemenet;
 
-    data.filter((element) => {
-        if(element.id == randomID){
-            randomElemenet = element
-        }
-    })
+        data.filter((element) => {
+            if (element.id == randomID) {
+                randomElemenet = element
+            }
+        })
 
-    console.log(randomElemenet)
+        console.log(randomElemenet)
 
-    res.status(200).json(randomElemenet)
+        res.status(200).json(randomElemenet)
+
+    }
 
 }
 
-export { GetHome, GetLanguages }
+let GetCourseById = (req, res) => {
+
+    let course = req.params.courseId
+
+    let courseData;
+
+    data.filter((element) => {
+        if (element.id == course) {
+            courseData = element
+        }
+    })
+
+    if (courseData) {
+        res.status(200).json(courseData)
+    } else {
+        res.status(404).json({ "Message": "Searched Course not found !" })
+    }
+
+}
+
+let PostAddCourse = (req,res) =>{
+    console.log(req.body)
+
+    let newCourse = { ...req.body, id: data[data.length - 1].id + 1 }
+
+    data.push(newCourse)
+
+    res.status(201).json({"message":"Successfully Added Course !"})
+}
+
+let DeleteCourse = (req,res) =>{
+    let whatToDelete = req.params.deleteCourseId
+
+    data = data.filter((element)=>{
+        return element.id != whatToDelete
+    })
+
+    res.status(200).json({"message":"Successfully Delete Course !"})
+
+}
+
+export { GetHome, GetLanguages, GetCourseById , PostAddCourse , DeleteCourse }
--- backend-apis/routers/router.js
+++ backend-apis/routers/router.js
@@ -1,11 +1,23 @@
 import express from "express"
 
-import { GetHome , GetLanguages } from "../controllers/controller.js"
+import { GetHome , GetLanguages , GetCourseById , PostAddCourse ,DeleteCourse } from "../controllers/controller.js"
 
 let router = express()
 
 router.get("/", GetHome)
 
+// This route is for query pramas 
 router.get('/languages', GetLanguages)
+
+// this route is for path pramas
+router.get('/course/:courseId' , GetCourseById)
+
+// this is api post route
+
+router.post("/addcourse",PostAddCourse)
+
+// this is delete api route
+
+router.delete("/deletecourse/:deleteCourseId",DeleteCourse)
 
 export { router }
